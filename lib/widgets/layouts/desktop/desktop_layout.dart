951dda9f48873ec1c0f238c1bd4e43538ce3d0b8
George-Abboud <geo1111ang@gmail.com>
2024-10-30 20:32:21 +0300

    create user info list tile card and fix fon family problem

--- lib/widgets/layouts/desktop/desktop_layout.dart
+++ lib/widgets/layouts/desktop/desktop_layout.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/common/custom_drawer.dart';
 
 class DesktopLayout extends StatelessWidget {
   const DesktopLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Row(
+        children: [
+          Expanded(
+            child: CustomDrawer(),
+          ),
+        ],
+      ),
+    );
   }
 }
