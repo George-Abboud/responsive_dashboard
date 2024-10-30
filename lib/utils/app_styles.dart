0b11310f48376b4d9483cf0578b9d7833c79ba54
George-Abboud <geo1111ang@gmail.com>
2024-10-30 19:15:06 +0300

    create styles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  //Regular:
+  static const styleRegular12 = TextStyle(
+    color: Color(0xFFAAAAAA),
+    fontSize: 12,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+
+  static const styleRegular14 = TextStyle(
+    color: Color(0xFFAAAAAA),
+    fontSize: 14,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+
+  static const styleRegular16 = TextStyle(
+    color: Color(0xFF064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+
+//--------------------------------------------------------------------------------
+
+//Medium:
+  static const styleMedium16 = TextStyle(
+    color: Color(0xFF064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+
+  static const styleMedium20 = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+
+//--------------------------------------------------------------------------------
+
+//SemiBold:
+  static const styleSemiBold16 = TextStyle(
+    color: Color(0xFF064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+  static const styleSemiBold18 = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 18,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+  static const styleSemiBold20 = TextStyle(
+    color: Color(0xFF064060),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+  static const styleSemiBold24 = TextStyle(
+    color: Color(0xFF4EB7F2),
+    fontSize: 24,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+//--------------------------------------------------------------------------------
+
+//Bold:
+  static const styleBold16 = TextStyle(
+    color: Color(0xFF064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+}
