653bcb7525c2876ed4722715076ffb205e2e873a
George-Abboud <geo1111ang@gmail.com>
2024-11-04 15:00:44 +0300

    create title text field

---
+++ lib/core/widgets/expenses&invoices/invoices/body/custom_text_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/constants.dart';
+import 'package:responsive_dashboard/core/utils/app_styles.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hint});
+  final String hint;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: AppStyles.styleRegular16.copyWith(color: kSubtitlesColor),
+        filled: true,
+        fillColor: kBackGroundItemColor,
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: const BorderSide(color: kBackGroundItemColor),
+    );
+  }
+}
--- lib/core/widgets/expenses&invoices/invoices/body/quick_invoices_body.dart
+++ lib/core/widgets/expenses&invoices/invoices/body/quick_invoices_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/core/utils/app_styles.dart';
 import 'package:responsive_dashboard/core/widgets/expenses&invoices/invoices/body/latest_transaction.dart';
+import 'package:responsive_dashboard/core/widgets/expenses&invoices/invoices/body/title_text_field.dart';
 
 class QuickInvoicesBody extends StatelessWidget {
   const QuickInvoicesBody({super.key});
@@ -18,6 +19,14 @@ class QuickInvoicesBody extends StatelessWidget {
           height: 12,
         ),
         LatestTransaction(),
+        Divider(
+          color: Color(0xFFF1F1F1),
+          height: 48,
+        ),
+        TitleTextField(
+          title: 'Customer name',
+          hint: 'Type customer name',
+        ),
       ],
     );
   }
